e2388e83cb952b81fe2874d686485b339d184c07
Brocman <roman.brocman@icloud.com>
2026-01-25 16:33:09 +0700

    Update process.py

--- src/process.py
+++ src/process.py
@@ -1,6 +1,7 @@
 import re
 import requests
 from pathlib import Path
+from datetime import datetime
 
 HOSTS_URL = "https://raw.githubusercontent.com/Internet-Helper/GeoHideDNS/refs/heads/main/hosts/hosts"
 
@@ -24,16 +25,30 @@ def build_regex(domains):
 
 
 def main():
+    # читаем домены
     domains = DOMAINS_FILE.read_text(encoding="utf-8").splitlines()
     regex = build_regex(domains)
 
+    # скачиваем upstream hosts
     response = requests.get(HOSTS_URL, timeout=20)
     response.raise_for_status()
 
+    # ищем совпадения
     matches = regex.findall(response.text)
 
+    # дата генерации
+    generated_date = datetime.utcnow().strftime("%Y-%m-%d")
+
+    # формируем файл hosts
+    content = [
+        f"# Generated: {generated_date}",
+        "",
+    ]
+
+    content.extend(f"{ip} {domain.lower()}" for ip, domain in matches)
+
     OUTPUT_FILE.write_text(
-        "\n".join(f"{ip} {domain.lower()}" for ip, domain in matches),
+        "\n".join(content),
         encoding="utf-8"
     )
 
